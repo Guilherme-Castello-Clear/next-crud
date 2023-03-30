f35af18bc3265c94a6d630f197605e0521196989
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-30 08:49:10 -0300

    Feat: Loading data in clients html table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
+import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 19, '2'),
+    new Cliente('Maria', 49, '3'),
+    new Cliente('Rosa', 51, '4'),   
+
+  ]
+
   return (
     <div className={`
       flex h-screen justify-center items-center
-      bg-gradient-to-r from-purple-500 to-blue-600
+      bg-gradient-to-r from-blue-600 to-purple-500
+      text-white
     `}>
-      <span className='text-4xl'>Texto</span>
+      <Layout titulo='Cadastro Simples'>
+        <Tabela clientes={clientes}></Tabela>
+      </Layout>
+
     </div>
   )
 }
